75f16b77ffad9703c0a90807482dae1b4fafaac9
Simon Chen <simonhchen@gmail.com>
2020-04-15 11:47:14 -0700

    added Light class

--- BCDG-3D.py
+++ BCDG-3D.py
@@ -47,6 +47,34 @@ class Block(Cube):
         z += Block.speed * dt
         self.position = x, y, z
 
+class Light(object):
+    enabled = False
+    colors = [(1., 1., 1., 1.), (1., 0.5, 0.5, 1.),
+              (0.5, 1., 0.5, 1.), (0.5, 0.5, 1., 1.)]
+
+    def __init__(self, light_id, position):
+        self.light_id = light_id
+        self.position = position
+        self.current_color = 0
+
+    def render(self):
+        light_id = self.light_id
+        color = Light.colors[self.current_color]
+        glLightfv(light_id, GL_POSITION, self.position)
+        glLightfv(light_id, GL_DIFFUSE, color)
+        glLightfv(light_id, GL_CONSTANT_ATTENUATION, 0.1)
+        glLightfv(light_id, GL_LINEAR_ATTENUATION, 0.05)
+
+    def switch_color(self):
+        self.current_color += 1
+        self.current_color %= len(Light.colors)
+
+    def enable(self):
+        if not Light.enabled:
+            glEnable(GL_LIGHTING)
+            Light.enable = True
+        glEnable(self.light_id)
+
 
 class Sphere(object):
     slices = 40
